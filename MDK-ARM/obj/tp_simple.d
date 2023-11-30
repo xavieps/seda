643087f993834b19df70b6a53cf2042d32d8f257
Javier Pérez <j.perezalonso@edu.uah.es>
2023-11-30 17:01:07 +0100

    holo world

---
+++ MDK-ARM/obj/tp_simple.d
@@ -0,0 +1,11 @@
+.\obj\tp_simple.o: ..\USER\GIEC\tp_simple.c
+.\obj\tp_simple.o: ..\Libraries\CM3\DeviceSupport\LPC17xx.H
+.\obj\tp_simple.o: ..\Libraries\CM3\CoreSupport\core_cm3.h
+.\obj\tp_simple.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\obj\tp_simple.o: ..\Libraries\CM3\CoreSupport\core_cmInstr.h
+.\obj\tp_simple.o: ..\Libraries\CM3\CoreSupport\core_cmFunc.h
+.\obj\tp_simple.o: ..\Libraries\CM3\DeviceSupport\system_LPC17xx.h
+.\obj\tp_simple.o: ..\Libraries\GLCD\glcd.h
+.\obj\tp_simple.o: ..\Libraries\TouchPanel\TouchPanel.h
+.\obj\tp_simple.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\obj\tp_simple.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
